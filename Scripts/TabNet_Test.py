1130d80ba18506f1332ee9f3b775e48b2fe0a1cc
Ivan Reznikov <Rezivan@icloud.com>
2025-08-07 01:39:17 -0400

    fixed NB code

--- Scripts/TabNet_Test.py
+++ Scripts/TabNet_Test.py
@@ -1,5 +1,5 @@
 
-import tensorflow as tf
+#import tensorflow as tf
 import numpy as np
 import pandas as pd
 import sklearn.model_selection
@@ -26,16 +26,16 @@ y = df['Level']
 sum = 0
 
 for i in range(10):
-    X_train, X_evaluate, y_train, y_evaluate = train_test_split(X,y,test_size = .2, shuffle=True)
-    X_val, X_test, y_val, y_test = train_test_split(X_evaluate, y_evaluate, test_size=.5, shuffle=True)
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = .2, shuffle=True)
+    #X_val, X_test, y_val, y_test = train_test_split(X_evaluate, y_evaluate, test_size=.5, shuffle=True)
     npX_train = X_train.values
-    npX_val = X_val.values
+    #npX_val = X_val.values
     npX_test = X_test.values
-    npX_evaluate = X_evaluate.values
+    #npX_evaluate = X_evaluate.values
     npy_train = y_train.values
-    npy_val = y_val.values
+    #npy_val = y_val.values
     npy_test = y_test.values
-    npY_evaluate = y_evaluate.values
+    #npY_evaluate = y_evaluate.values
 
     '''
     TorchX_train = torch.from_numpy(npX_train)
@@ -43,7 +43,7 @@ for i in range(10):
     '''
 
     clf = TabNetClassifier(n_a=22, n_d=8, n_steps=2, optimizer_fn=torch.optim.Adam, optimizer_params=dict(lr=0.0375))
-    clf.fit(npX_train, npy_train, batch_size=100, virtual_batch_size=10, eval_set=[(npX_val, npy_val)], max_epochs=100, eval_metric = ['accuracy'], compute_importance=True)
+    clf.fit(npX_train, npy_train, batch_size=100, virtual_batch_size=10, max_epochs=100, eval_metric = ['accuracy'], compute_importance=True) #eval_set=[(npX_val, npy_val)]
     #featureimportances = clf.feature_importances_
     print(npy_test)
     preds = clf.predict(npX_test)
